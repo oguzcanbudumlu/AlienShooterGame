185a1b36a1eeae0c77849b43fdc578135e6a2221
cetinomer <omer.cetin.std@gmail.com>
2018-11-15 17:42:48 +0300

    Score class added NOW, we can get the leaderboard for all the time and for the last week.
    Note, in the last commit the score class was not added.

---
+++ src/main/java/alienshooterserver/alienshooterserverside/score/Score.java
@@ -0,0 +1,29 @@
+package alienshooterserver.alienshooterserverside.score;
+
+public class Score {
+    private String name;
+    private long score;
+
+    Score() {}
+
+    public Score(String name, long score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getScore() {
+        return score;
+    }
+
+    public void setScore(long score) {
+        this.score = score;
+    }
+}
