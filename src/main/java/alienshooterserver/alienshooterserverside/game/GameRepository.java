ff224895de0630b5b738107daa500e9bf1de67d5
= <=>
2018-12-16 01:32:37 +0300

    files organized

--- src/main/java/alienshooterserver/alienshooterserverside/game/GameRepository.java
+++
@@ -1,6 +0,0 @@
-package alienshooterserver.alienshooterserverside.game;
-
-import org.springframework.data.repository.CrudRepository;
-
-//CrudRepository provides generic CRUD operations
-public interface GameRepository extends CrudRepository<Game, Long> {}
